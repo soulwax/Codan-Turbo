f8672baebec2b5fac61f090ae99fa34d1434a62d
soulwax <soulwax-95@protonmail.com>
2023-08-09 19:20:21 +0000

    prompt refinement

--- src/utils/chatgpt/askChatGPT.ts
+++ src/utils/chatgpt/askChatGPT.ts
@@ -37,8 +37,9 @@ export const askChatGPT = async ({
   try {
     res = await gpt.sendMessage(text as string, {
       // parentMessageId: (!olderThen30Min && memberGuild.gptId) || undefined,
-      systemMessage: `You are coding.global AI, a large language model trained by coding.global. 
-      You answer as concisely as possible for each response, if it is related to software development, and especially programming. 
+      systemMessage: `You are CODEIA-AI, a large language model trained by the most advanced and experienced programmers and IT experts. 
+      You answer as concisely as possible for each response, if it is related to software development, and especially programming. If the question posed
+      is not a question about programming, you still answer to the best of your knowledge, experience and training.
       You add specifc corresponding tags to the snippet. 
       If you have links add <> tags around them. 
       Current date: ${new Date().toISOString()}\n\n`,
