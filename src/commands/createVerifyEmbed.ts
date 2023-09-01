760caadff62a8c2e4a1747a7f0befdd2d1023257
soulwax <soulwax-95@protonmail.com>
2023-09-01 02:44:34 +0200

    feat: Update environment variables and command behavior
    - Whitelist of channels for bot interaction has been updated
    - Added a new whitelist for verify embeds
    - Added an option to restrict the bot to a single server

--- src/commands/createVerifyEmbed.ts
+++ src/commands/createVerifyEmbed.ts
@@ -1,36 +1,36 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { PermissionFlagsBits } from 'discord-api-types/v9';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { PermissionFlagsBits } from "discord-api-types/v9";
 import type {
   APIEmbed,
   CacheType,
   CommandInteraction,
   Message,
   TextChannel,
-} from 'discord.js';
+} from "discord.js";
 import {
   BOT_ICON,
   RED_COLOR,
-  VERIFY_CHANNEL,
+  VERIFY_CHANNELS,
   VERIFY_TEMPLATE,
-} from '../utils/constants.js';
+} from "../utils/constants.js";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('create-verify-embed')
-    .setDescription('verify all users in the server')
+    .setName("create-verify-embed")
+    .setDescription("verify all users in the server")
     .setDefaultMemberPermissions(PermissionFlagsBits.DeafenMembers),
   async execute(interaction: CommandInteraction<CacheType>) {
     // get text channel
     const channel = (await interaction.channel?.fetch()) as TextChannel;
 
     // if not verify channel, return
-    if (channel.name !== VERIFY_CHANNEL) return;
+    if (VERIFY_CHANNELS.includes(channel.name) === false) return;
 
     // create verify embed
     const embed: APIEmbed = {
       color: RED_COLOR,
-      title: 'Verification process...',
-      description: `**--- :flag_gb: Welcome to our Coding Server! :flag_gb: ---**
+      title: "Verification process...",
+      description: `**--- :flag_gb: Welcome to the Imperial Cult! :flag_gb: ---**
 
 We are dedicated members, who are professionally and in their spare time engaged in programming and other IT topics.
 Most of the programmers here are trainees, students, apprentices or have started their own business in IT. 
@@ -40,7 +40,7 @@ We generally don't like it so much when the sole purpose of being here is to ans
 
 ------------------- 🙂 Happy Coding 🙂 -------------------
 
---- :flag_de: Herzlich Willkommen auf unserem Coding Server! :flag_de: ---
+--- :flag_de: Herzlich Willkommen auf unserem Imperialen Cult! :flag_de: ---
 
 Wir sind engagierte Member, die sich beruflich und in ihrer Freizeit mit dem Programmieren und weiteren IT Themen beschäftigen.
 Die meisten hier anzutreffenden Programmierenden sind Azubis, Studenten, Ausgelernte, oder haben sich in der IT selbstständig gemacht. 
@@ -66,6 +66,6 @@ add 👍 to get verified
     });
 
     // add reactions for verify role
-    (message as Message<boolean>).react('👍');
+    (message as Message<boolean>).react("👍");
   },
 };
--- src/utils/messages/cleanUpVerifyChannel.ts
+++ src/utils/messages/cleanUpVerifyChannel.ts
@@ -1,11 +1,11 @@
 import { Message, MessageType, TextChannel } from 'discord.js';
-import { VERIFY_CHANNEL } from '../constants.js';
+import { VERIFY_CHANNELS } from '../constants.js';
 
 export const cleanUpVerifyChannel = async (message: Message<boolean>) => {
   const channel = (await message.channel?.fetch()) as TextChannel;
   // remove non command messages in verify channel
   if (
-    channel.name === VERIFY_CHANNEL &&
+    VERIFY_CHANNELS.includes(channel.name) &&
     message.type !== MessageType.ChatInputCommand
   ) {
     message.delete();
